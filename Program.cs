34bd28c547d786f3ffabe6be15c79ada0b6b5790
Chalida <pattiralekwilai@gmail.com>
2023-02-27 00:25:17 +0700

    edit passcode system

--- Program.cs
+++ Program.cs
@@ -3,15 +3,13 @@
 namespace Assignment1 {
     class Program {
         static void Main(string[] args) {
-            Console.Write("Input your passcode (6 digits): ");
-            int passcode = int.Parse(Console.ReadLine());
-
-            if (passcode<1000000 && passcode>=99999) {
-                Console.Write("Input your affiliation: ");
-                string aff = Console.ReadLine();
-            } else {
-                Console.WriteLine("Incorrect passcode.");
-            }
+            Console.Write("Enter your passcode (6 digits): ");
+            int passcode1 = int.Parse(Console.ReadLine());
+            int passcode2 = int.Parse(Console.ReadLine());
+            int passcode3 = int.Parse(Console.ReadLine());
+            int passcode4 = int.Parse(Console.ReadLine());
+            int passcode5 = int.Parse(Console.ReadLine());
+            int passcode6 = int.Parse(Console.ReadLine());
         }
     }
 }
