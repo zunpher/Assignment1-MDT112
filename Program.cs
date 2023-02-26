293be1cdb8b8072e3010a59735eaf7a3e4c20194
Chalida <pattiralekwilai@gmail.com>
2023-02-27 00:10:48 +0700

    add passcode and aff system

--- Program.cs
+++ Program.cs
@@ -3,7 +3,15 @@
 namespace Assignment1 {
     class Program {
         static void Main(string[] args) {
-            
+            Console.Write("Input your passcode (6 digits): ");
+            int passcode = int.Parse(Console.ReadLine());
+
+            if (passcode<1000000 && passcode>=99999) {
+                Console.Write("Input your affiliation: ");
+                string aff = Console.ReadLine();
+            } else {
+                Console.WriteLine("Incorrect passcode.");
+            }
         }
     }
 }
