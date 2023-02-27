14d3d17a9ccb16e2129ce23a0f53d5cbf520bc98
Chalida <pattiralekwilai@gmail.com>
2023-02-27 16:59:07 +0700

    add digit control

--- Program.cs
+++ Program.cs
@@ -9,15 +9,20 @@ namespace Assignment1 {
             if (passcode > 99999 && passcode < 1000000) {
                 Console.Write("Enter your affiliation (CIA, FBI, NSA): ");
                 string aff = Console.ReadLine();
+                int digit1 = passcode % 10;
+                int dg2Ctrl = passcode / 10;
+                int digit2 = dg2Ctrl % 10;
+                int dg3Ctrl = passcode / 100;
+                int digit3 = dg3Ctrl % 10;
+                int dg4Ctrl = passcode / 1000;
+                int digit4 = dg4Ctrl % 10;
+                int dg5Ctrl = passcode / 10000;
+                int digit5 = dg5Ctrl % 10;
+                int dg6Ctrl = passcode / 100000;
+                int digit6 = dg6Ctrl % 10;
 
                 switch (aff) {
                     case "CIA" :
-                    int digit1 = passcode % 10;
-                    int dg2Ctrl = passcode / 10;
-                    int digit2 = dg2Ctrl % 10;
-                    int dg4Ctrl = passcode / 1000;
-                    int digit4 = dg4Ctrl % 10;
-
                     if (digit1 % 3 == 0) {
                         if (digit2 != 1 && digit2 != 3 && digit2 != 5) {
                             if (digit4 >= 6 && digit4 != 8) {
@@ -34,6 +39,7 @@ namespace Assignment1 {
                     break;
 
                     case "FBI" :
+                    
                     break;
 
                     case "NSA" :
