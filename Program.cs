9b46a52c591c3e24bed493f3f4427670fa389b81
Chalida <pattiralekwilai@gmail.com>
2023-02-27 16:11:29 +0700

    add aff system

--- Program.cs
+++ Program.cs
@@ -4,12 +4,14 @@ namespace Assignment1 {
     class Program {
         static void Main(string[] args) {
             Console.Write("Enter your passcode (6 digits): ");
-            int pass1 = Console.Read();
-            int pass2 = Console.Read();
-            int pass3 = Console.Read();
-            int pass4 = Console.Read();
-            int pass5 = Console.Read();
-            int pass6 = Console.Read();
+            int passcode = int.Parse(Console.ReadLine());
+
+            if (passcode > 99999 && passcode < 1000000) {
+                Console.Write("Enter your affiliation (CIA, FBI, NSA): ");
+                string aff = Console.ReadLine();
+            } else {
+                Console.WriteLine("False");
+            }
         }
     }
 }
