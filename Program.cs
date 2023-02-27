2a5c01289b02e5ac43e1016881862a8df9831675
Chalida <pattiralekwilai@gmail.com>
2023-02-27 16:39:36 +0700

    finish  CIA

--- Program.cs
+++ Program.cs
@@ -12,12 +12,35 @@ namespace Assignment1 {
 
                 switch (aff) {
                     case "CIA" :
+                    int digit1 = passcode % 10;
+                    int dg2Ctrl = passcode / 10;
+                    int digit2 = dg2Ctrl % 10;
+                    int dg4Ctrl = passcode / 1000;
+                    int digit4 = dg4Ctrl % 10;
+
+                    if (digit1 % 3 == 0) {
+                        if (digit2 != 1 && digit2 != 3 && digit2 != 5) {
+                            if (digit4 >= 6 && digit4 != 8) {
+                                Console.WriteLine("True");
+                            } else {
+                                Console.WriteLine("False");
+                            }
+                        } else {
+                            Console.WriteLine("False");
+                        }
+                    } else {
+                        Console.WriteLine("False");
+                    }
                     break;
 
                     case "FBI" :
                     break;
 
                     case "NSA" :
+                    break;
+
+                    default :
+                    Console.WriteLine("False");
                     break;
                 }
             } else {
